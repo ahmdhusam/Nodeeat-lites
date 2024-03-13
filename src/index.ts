176f8ff1e6acc43a76571707a61934db6b239685
abdoafage <abdo.afage2@gmail.com>
2024-03-13 18:16:12 +0200

    feat: setup docker compose for dev

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
+import httpStatus from "http-status";
+import morgan from "morgan";
+import winston from "winston";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan("dev"));
 
 // Routes
-app.use("/");
+app.use("/", (req: any, res: any) => {
+  res.send("HELLO");
+});
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
