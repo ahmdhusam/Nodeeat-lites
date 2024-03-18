7af2e5c4b99dd5caf725d6cdaa18a1000cfb24ea
abdoafage <abdo.afage2@gmail.com>
2024-03-18 17:47:46 +0200

    1- create addToCard and viewCard features. 2- create README file to show database scripts.

--- src/domain/controllers/cart.controller.ts
+++ src/domain/controllers/cart.controller.ts
@@ -2,17 +2,17 @@ import { Request, Response } from "express";
 import { cartRepository } from "../repositry/cart.repository";
 import { cartItemRepository } from "../repositry/cart-item.repository";
 
-export const getCart = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const cartId = parseInt(req.params.cartId);
-    let cart = await cartRepository.findOneById(cartId);
+// export const getCart = async (req: Request, res: Response): Promise<void> => {
+//   try {
+//     const cartId = parseInt(req.params.cartId);
+//     let cart = await cartRepository.findOneById(cartId);
 
-    console.log("cart controller");
-    res.status(200).json({ cart });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
-};
+//     console.log("cart controller");
+//     res.status(200).json({ cart });
+//   } catch (error: any) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
 export const deleteCartItem = async (
   req: Request,
--- src/domain/routes/cart.router.ts
+++ src/domain/routes/cart.router.ts
@@ -1,8 +1,15 @@
 import { Router, Request, Response } from "express";
-import { deleteCartItem, getCart } from "../controllers/cart.controller";
+import { deleteCartItem } from "../controllers/cart.controller";
+import {
+  addCartItem,
+  viewCartItems,
+} from "../controllers/cart-item.controller";
 
 export const router: Router = Router();
 
-router.route("/:cartId").get(getCart);
+router.route("/:cartId").get(viewCartItems);
 
-router.route("/:cartId/cart-Items/:cartItemId").delete(deleteCartItem);
+router
+  .route("/:cartId/cart-items/:cartItemId")
+  .delete(deleteCartItem)
+  .post(addCartItem);
